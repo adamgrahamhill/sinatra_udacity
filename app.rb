e4a5e8f14a70a9a70dbd0502e1b108adbe33b694
Adam Hill <adam.graham.hill@gmail.com>
2016-09-09 18:17:02 -0400

    Add two routes w/variable segments. Solve Udacity square quiz

--- app.rb
+++ app.rb
@@ -12,3 +12,19 @@ end
 get "/roll-die" do
 	"Your die roll is #{rand(1..6)}"
 end
+
+#first route with variable portion
+get "/greet/:name" do
+	"Hello #{params[:name]}!"
+end
+
+#square variable portion of route, account for non-numbers
+get "/square/:num" do
+  if params[:num] == "0"
+  	"0"
+  elsif params[:num].to_i != 0
+  	(params[:num].to_i ** 2).to_s  #return value must be a string or wouldn't display. Hence the .to_s
+  else
+  	"#{params[:num]} is not something you can square. Try entering a number!"
+  end
+end
